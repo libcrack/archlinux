3ca40df4fb69a43c2f6febb56dc6166b67c4c332
libcrack <libcrack@users.noreply.github.com>
2016-05-08 20:53:43 +0200

    New: Added apparmor profiles

---
+++ AppArmor-Profiles/home.harvie.Work.bash-offline.sh
@@ -0,0 +1,14 @@
+# Last Modified: Fri Jan 20 21:18:46 2012
+/home/harvie/Work/bash-offline.sh {
+  deny capability chown,
+  deny capability net_raw,
+  deny capability setgid,
+  deny capability setuid,
+  deny capability sys_ptrace,
+  deny capability sys_resource,
+
+
+
+  /** mrwlkix,
+
+}
